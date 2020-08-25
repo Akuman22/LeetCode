33343b3417f5318ad199fb2327e5295c3303e014
Akash Mehta <amehta22@bu.edu>
2020-08-25 01:46:40 -0500

    Create FruitIntoBaskets.java

---
+++ Google/FruitIntoBaskets.java
@@ -0,0 +1,78 @@
+/*
+In a row of trees, the i-th tree produces fruit with type tree[i].
+
+You start at any tree of your choice, then repeatedly perform the following steps:
+
+Add one piece of fruit from this tree to your baskets.  If you cannot, stop.
+Move to the next tree to the right of the current tree.  If there is no tree to the right, stop.
+Note that you do not have any choice after the initial choice of starting tree: you must perform step 1, then step 2, then back to step 1, then step 2, and so on until you stop.
+
+You have two baskets, and each basket can carry any quantity of fruit, but you want each basket to only carry one type of fruit each.
+
+What is the total amount of fruit you can collect with this procedure?
+
+ 
+
+Example 1:
+
+Input: [1,2,1]
+Output: 3
+Explanation: We can collect [1,2,1].
+Example 2:
+
+Input: [0,1,2,2]
+Output: 3
+Explanation: We can collect [1,2,2].
+If we started at the first tree, we would only collect [0, 1].
+Example 3:
+
+Input: [1,2,3,2,2]
+Output: 4
+Explanation: We can collect [2,3,2,2].
+If we started at the first tree, we would only collect [1, 2].
+Example 4:
+
+Input: [3,3,3,1,2,1,1,2,3,3,4]
+Output: 5
+Explanation: We can collect [1,2,1,1,2].
+If we started at the first tree or the eighth tree, we would only collect 4 fruits.
+ 
+
+Note:
+
+1 <= tree.length <= 40000
+0 <= tree[i] < tree.length
+*/
+
+class Solution {
+    public int totalFruit(int[] tree) {
+        Integer bucket1 = tree[0];
+        Integer bucket2 = null;
+        int currTree;
+        int refIndex = 1;
+        int i = 1;
+        int maxima = 1;
+        int tempMaxima = 1;
+        int N = tree.length;
+        while(i < N) {
+            currTree = tree[i];
+            if(bucket2 == null && currTree != bucket1) {
+                bucket2 = currTree;
+                refIndex = i;
+            } else if(currTree == bucket1 || currTree == bucket2) {
+                if(tree[i-1] != currTree) refIndex = i;
+            } else {
+                if(tempMaxima > maxima) maxima = tempMaxima;
+                i = refIndex + 1;
+                tempMaxima = 1;
+                bucket1 = tree[refIndex];
+                bucket2 = null;
+                continue;
+            }
+            i++;
+            tempMaxima++;
+        }
+        if(tempMaxima > maxima) maxima = tempMaxima;
+        return maxima;
+    }
+}
